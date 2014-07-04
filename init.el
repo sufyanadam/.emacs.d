f8c41a28946e795e70201750998ed172dd7ca4dc
Sufyan Adam <sufyan.adam@gmail.com>
2014-07-04 11:41:38 -0700

    Bind C-c e to toggle evil mode

--- init.el
+++ init.el
@@ -21,6 +21,9 @@
 (global-set-key (kbd "H-l") (Λ (insert "\u03bb")))
 (global-set-key (kbd "H-L") (Λ (insert "\u039B")))
 
+;; Toggle EVIL mode
+(global-set-key (kbd "C-c e") (Λ (evil-mode 'toggle)))
+
 ;; Settings for currently logged in user
 (setq user-settings-dir
       (concat user-emacs-directory "users/" user-login-name))
