ff4578fd167ff24eb97e647af3e2ce61c2e03c2e
Pablo Kang and Sufyan Adam <sufyan.adam@goodsearch.com>
2014-09-12 10:19:44 -0700

    New personal keybindings

--- personal-keybindings.el
+++ personal-keybindings.el
@@ -1,2 +1,8 @@
+(bind-key "C-c C-c" 'comment-or-uncomment-current-line-or-region)
+(bind-key "C-c C-g" 'magit-status)
+(bind-key "C-c C-l" 'magit-file-log)
+
+(bind-key "C-c C-f" 'projectile-find-file)
+(bind-key "C-c C-t" 'projectile-toggle-between-implementation-and-test)
 
 (provide 'personal-keybindings)
