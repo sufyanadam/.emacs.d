b9df5bb0b1be7f0213b80b06bad376dc4f5c6901
Sufyan Adam <sufyan.adam@gmail.com>
2014-12-28 19:21:32 -0800

    shell path needs to be correct for magit to work correctly. Otherwise you get an error. Unfortunately, this adds almost a second to boot time

--- init.el
+++ init.el
@@ -46,8 +46,8 @@
 (when is-mac
   ;; Setup environment variables from the user's shell
   (use-package exec-path-from-shell
-    :idle (exec-path-from-shell-initialize))
-  )
+    :init
+    (exec-path-from-shell-initialize)))
 
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
