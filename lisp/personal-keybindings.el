fef8ddf853fa2cc58499461b04fa60e1cf0311b8
Bronson McKinley and Sufyan Adam <bronson.mckinley@goodsearch.com>
2014-12-30 11:16:02 -0800

    Add custom keybindings for ag and other useful text operations

--- lisp/personal-keybindings.el
+++ lisp/personal-keybindings.el
@@ -2,12 +2,15 @@
 (bind-key "C-c C-g" 'magit-status)
 (bind-key "C-c C-l" 'magit-file-log)
 
-(bind-key "M-[" 'er/contract-region)
-(bind-key "M-]" 'er/expand-region)
+(bind-key "M-[" 'er/expand-region)
+(bind-key "M-]" 'er/contract-region)
 
 (bind-key "C-c f" 'projectile-find-file)
 (bind-key "C-c C-t" 'projectile-toggle-between-implementation-and-test)
 (bind-key "C-c r" 'eval-and-replace)
 (bind-key "C-c c" 'inf-ruby-console-auto)
+(bind-key "C-c ag" 'ag)
+(bind-key "C-c d" 'duplicate-current-line-or-region)
+(bind-key "C-c '" 'toggle-quotes)
 
 (provide 'personal-keybindings)
