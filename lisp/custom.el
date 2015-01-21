72ff83417699af5d9271cf3642fb4c165256d770
Sufyan Adam <pair+sufyanadam@gmail.com>
2015-01-20 22:14:58 -0800

    Better indentation in enh-ruby-mode

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,7 +3,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("f0ea6118d1414b24c2e4babdc8e252707727e7b4ff2e791129f240a2b3093e32" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" "7fbb8d064286706fb1e319c9d3c0a8eafc2efe6b19380aae9734c228b05350ae" default)))
+ '(custom-safe-themes
+   (quote
+    ("f0ea6118d1414b24c2e4babdc8e252707727e7b4ff2e791129f240a2b3093e32" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" "7fbb8d064286706fb1e319c9d3c0a8eafc2efe6b19380aae9734c228b05350ae" default)))
+ '(enh-ruby-bounce-deep-indent nil)
+ '(enh-ruby-deep-arglist nil)
+ '(enh-ruby-deep-indent-paren nil)
+ '(enh-ruby-hanging-brace-deep-indent-level 0)
+ '(enh-ruby-hanging-brace-indent-level 2)
  '(js2-basic-offset 2)
  '(js2-highlight-level 3)
  '(js2-missing-semi-one-line-override t)
@@ -14,6 +21,7 @@
  '(org-hide-leading-stars t)
  '(org-pretty-entities t)
  '(org-src-fontify-natively t t)
+ '(ruby-deep-indent-paren nil)
  '(scss-compile-at-save nil))
  '(custom-safe-themes (quote ("c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "0ebe0307942b6e159ab794f90a074935a18c3c688b526a2035d14db1214cf69c" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" "ce79400f46bd76bebeba655465f9eadf60c477bd671cbcd091fe871d58002a88" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "dd43c9f997208c61ce0f4855932cc20a57ae2f37fe2ced218dace5c8c321d1e8" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
 (custom-set-faces
