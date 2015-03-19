0d71c1ea49effcbefd83fb494ce292cb33c7e431
Sufyan Adam <pair+sufyanadam@gmail.com>
2015-03-19 09:22:32 -0700

    Customizations for web-mode

--- lisp/custom.el
+++ lisp/custom.el
@@ -22,11 +22,20 @@
  '(org-pretty-entities t)
  '(org-src-fontify-natively t t)
  '(ruby-deep-indent-paren nil)
- '(scss-compile-at-save nil))
+ '(scss-compile-at-save nil)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-enable-auto-closing t)
+ '(web-mode-enable-auto-indentation t)
+ '(web-mode-markup-indent-offset 2)
+ '(web-mode-script-padding 2)
+ '(web-mode-sql-indent-offset 2)
+ '(web-mode-style-padding 2))
  '(custom-safe-themes (quote ("c7359bd375132044fe993562dfa736ae79efc620f68bab36bd686430c980df1c" "53e29ea3d0251198924328fd943d6ead860e9f47af8d22f0b764d11168455a8e" "0ebe0307942b6e159ab794f90a074935a18c3c688b526a2035d14db1214cf69c" "33c5a452a4095f7e4f6746b66f322ef6da0e770b76c0ed98a438e76c497040bb" "ce79400f46bd76bebeba655465f9eadf60c477bd671cbcd091fe871d58002a88" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "dd43c9f997208c61ce0f4855932cc20a57ae2f37fe2ced218dace5c8c321d1e8" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:background nil))))
  '(js2-private-function-call ((t (:foreground "goldenrod")))))
