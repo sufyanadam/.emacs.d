5d283cd5b2643c7c019c08f13329bc534c177abf
Sufyan Adam <pair+sufyanadam@gmail.com>
2015-07-01 22:05:45 -0700

    Remove reference to deprecated variable

--- init.el
+++ init.el
@@ -371,7 +371,6 @@
   :config
   (progn
     ;; Subtler highlight
-    (set-face-background 'magit-item-highlight "#121212")
     (set-face-background 'diff-file-header "#121212")
     (set-face-foreground 'diff-context "#666666")
     (set-face-foreground 'diff-added "#00cc33")
