59a2e458bfbe0a1bde89bc4dc31197079b523d75
Sufyan Adam <pair+sufyanadam@gmail.com>
2016-02-07 17:26:54 -0800

    Fix welcome message bug
    Needed to evaluate the return of the element retrieved from
    the list in case it was itself a list.

--- init.el
+++ init.el
@@ -55,20 +55,22 @@
 (autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
-(setq words-of-wisdom '(";; Let the hacking commence!"
-                       ";; Hacks and glory await!"
-                       ";; Hack and be merry!"
-                       ";; Your hacking starts... NOW!"
-                       ";; May the source be with you!"
-                       (format ";; %s, this could be the start of a beautiful program." (capitalize (user-login-name))))
-)
+(setq words-of-wisdom '(
+                        ";; Let the hacking commence!"
+                        ";; Hacks and glory await!"
+                        ";; Hack and be merry!"
+                        ";; Your hacking starts... NOW!"
+                        ";; May the source be with you!"
+                        (format ";; %s, this could be the start of a beautiful program." (capitalize (user-login-name)))
+                        ))
 
 (add-hook 'emacs-startup-hook
           (λ ()
             (when (string= (buffer-name) "*scratch*")
               (animate-string
-               (nth (% (random 10) (length words-of-wisdom)) words-of-wisdom)
-               (/ (frame-height) 2)))))
+               (eval (nth (% (random 10) (length words-of-wisdom)) words-of-wisdom))
+               (/ (frame-height) 2))
+              )))
 
 ;; personal defuns
 (use-package editing-defuns)
